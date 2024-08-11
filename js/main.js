d469e701dde60264f77d1490f65deae4b274129b
Teymur <melikovteymur@mail.ru>
2024-08-11 18:31:54 +0400

    food style tabs

---
+++ js/main.js
@@ -0,0 +1,37 @@
+document.addEventListener ("DOMContentLoaded", () => {
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsParent = document.querySelector('.tabheader__items'),
+          tabsContent = document.querySelectorAll('.tabcontent');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.remove('show', 'fade-in');
+            item.classList.add('hide');
+        });
+        tabs.forEach(item => {
+            item.classList.remove("tabheader__item_active");
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.remove('hide');
+        tabsContent[i].classList.add('show', 'fade-in');
+        tabs[i].classList.add("tabheader__item_active");
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener ("click", (e) => {
+        const target = e.target;
+
+        if (target && target.classList.contains("tabheader__item")) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+});
