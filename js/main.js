d25bde15b7d126d377f34199146febb63bcc0768
Teymur <melikovteymur@mail.ru>
2024-08-12 23:58:46 +0400

    add class MenuCard

--- js/main.js
+++ js/main.js
@@ -134,7 +134,7 @@ document.addEventListener ("DOMContentLoaded", () => {
         }
     });
 
-    const modalTimeoutId = setTimeout(openModal, 6000);
+    // const modalTimeoutId = setTimeout(openModal, 6000);
 
     function showModalByScroll() {
         if (window.scrollY + document.documentElement.clientHeight == document.documentElement.scrollHeight) {
@@ -144,4 +144,67 @@ document.addEventListener ("DOMContentLoaded", () => {
     }
 
     window.addEventListener('scroll', showModalByScroll);
+
+    //Classes for cards
+
+    class MenuCard {
+        constructor (img, alt, title, text, price, parentSelector) {
+            this.img = img;
+            this.alt = alt;
+            this.title = title;
+            this.text = text;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 41;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+                <div class="menu__item">
+                    <img src=${this.img} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.text}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        6,
+        '.menu .container'
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню "Премиум"',
+        "В меню “Премиум” не только привлекательный дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты — меню, позволяющее насладиться изысканным ужином, не выходя из дома!",
+        13,
+        '.menu .container'
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
+        10,
+        '.menu .container'
+    ).render();
 });
